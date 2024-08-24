bed56b19a10d6d1d9c0b5c602cf195d5742bfeae
arthrok <arthuralves1538@gmail.com>
2024-08-24 08:55:43 -0300

    first commit

---
+++ spark/src/myjob.py
@@ -0,0 +1,25 @@
+from pyspark.sql import SparkSession
+import time
+
+# Crie uma SparkSession
+spark = SparkSession.builder \
+    .appName("Soma Valor Custo Transferencia") \
+    .getOrCreate()
+
+# Carregue o arquivo CSV em um DataFrame
+df = spark.read.csv("/opt/spark/data/br_me_sic_transferencia.csv", header=True, inferSchema=True)
+
+# Registre o DataFrame como uma tabela temporária
+df.createOrReplaceTempView("transferencias")
+
+# Use Spark SQL para somar a coluna 'valor_custo_transferencia'
+resultado = spark.sql("SELECT SUM(valor_custo_transferencia) AS total_custo_transferencia FROM transferencias")
+
+# Exiba o resultado
+resultado.show()
+
+time.sleep(1000000)
+
+
+# Feche a SparkSession
+spark.stop()
